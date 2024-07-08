f9c299990809933ab1852448e88215f40adc060e
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-08 20:12:59 +0300

    Update model-families.js

--- src/api/model-family/middlewares/model-families.js
+++ src/api/model-family/middlewares/model-families.js
@@ -1,13 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * `model-families` middleware
  */
 
+const populate = {
+  brands: {
+    populate: true,
+  },
+};
+
 module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
-    strapi.log.info('In model-families middleware.');
+    strapi.log.info("In model-families middleware.");
+    ctx.query = { populate, ...ctx.query };
 
     await next();
   };
