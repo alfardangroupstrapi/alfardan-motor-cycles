819840cdc034b8ce64285725713b3084bc76d809
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-08 11:52:34 +0300

    Update lifestyles.js

--- src/api/lifestyle/middlewares/lifestyles.js
+++ src/api/lifestyle/middlewares/lifestyles.js
@@ -24,11 +24,47 @@ const populate = {
 };
 
 module.exports = (config, { strapi }) => {
-  // Add your own logic here.
   return async (ctx, next) => {
-    strapi.log.info("In lifestyles middleware.");
-    ctx.query = { populate, ...ctx.query };
+    strapi.log.info("In lifestyles-populate middleware.");
+    const { query } = ctx;
+    strapi.log.info(`Query before modification: ${JSON.stringify(query)}`);
 
+    const filters = [];
+
+    if (query["brand-id"]) {
+      const brandId = query["brand-id"];
+      delete query["brand-id"];
+      filters.push({ brand: { id: brandId } });
+    }
+
+    if (query["cat-id"]) {
+      const catId = query["cat-id"];
+      delete query["cat-id"];
+      filters.push({ category: { id: catId } });
+    }
+
+    if (query["subcat-id"]) {
+      const subcatId = query["subcat-id"];
+      delete query["subcat-id"];
+      filters.push({ subCategory: { id: subcatId } });
+    }
+
+    if (filters.length > 0) {
+      query.filters = {
+        $and: [query.filters || {}, ...filters],
+      };
+    }
+
+    // Add the populate object to the query
+    if (!query.populate) {
+      query.populate = populate;
+    } else {
+      query.populate = { ...query.populate, ...populate };
+    }
+
+    ctx.query = query;
+    strapi.log.info(`Query after modification: ${JSON.stringify(ctx.query)}`);
     await next();
   };
 };
+
