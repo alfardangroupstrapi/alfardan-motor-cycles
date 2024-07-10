f2fa74e2b53754266a8a30c30b8d9dba5280d481
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-10 14:47:38 +0300

    home landing page file uploads

---
+++ src/api/home-landing-page/routes/home-landing-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-landing-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-landing-page.home-landing-page');
---
+++ src/api/home-landing-page/services/home-landing-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-landing-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-landing-page.home-landing-page');
