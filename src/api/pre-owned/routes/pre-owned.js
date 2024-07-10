276bfa472f60ba5f9d475120bc8bf91438c719c4
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-10 11:00:03 +0300

    Pre-Owned files uploading

---
+++ src/api/pre-owned/routes/pre-owned.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pre-owned router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pre-owned.pre-owned');
---
+++ src/api/pre-owned/services/pre-owned.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pre-owned service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::pre-owned.pre-owned');
