5b02b5f5cb7987c0e231e6f2f76de07ae85a8f4c
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-11 06:25:31 +0300

    Delete src/api/home-landing-page directory

--- src/api/home-landing-page/controllers/home-landing-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * home-landing-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::home-landing-page.home-landing-page');
