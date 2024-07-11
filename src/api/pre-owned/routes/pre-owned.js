c6401a76c3912704b90496bb8b56eb9cd8dc531c
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-11 13:27:04 +0300

    Update pre-owned.js 2

--- src/api/pre-owned/routes/pre-owned.js
+++ src/api/pre-owned/routes/pre-owned.js
@@ -6,4 +6,13 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::pre-owned.pre-owned');
+module.exports = createCoreRouter('api::pre-owned.pre-owned', {
+  config: {
+    find: {
+      middlewares: ["api::pre-owned.pre-owneds"],
+    },
+    findOne: {
+      middlewares: ["api::pre-owned.pre-owneds"],
+    },
+  },
+});
