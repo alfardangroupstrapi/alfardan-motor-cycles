fba657ff6ac0e393a9502ae0361569495de6b4e6
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-15 14:06:23 +0300

    Update home-landing-page.js 1

--- src/api/home-landing-page/routes/home-landing-page.js
+++ src/api/home-landing-page/routes/home-landing-page.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::home-landing-page.home-landing-page');
+module.exports = createCoreRouter("api::home-landing-page.home-landing-page", {
+  config: {
+    find: {
+      middlewares: ["api::home-landing-page.home-landing-page"],
+    },
+  },
+});
