32b41999b94594101681addd2d1b04be1c55e61d
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-07-21 17:51:35 +0300

    Update list-of-models.js 1

--- src/api/list-of-model/middlewares/list-of-models.js
+++ src/api/list-of-model/middlewares/list-of-models.js
@@ -39,23 +39,24 @@ module.exports = (config, { strapi }) => {
     const { query } = ctx;
     strapi.log.info(`Query before modification: ${JSON.stringify(query)}`);
 
-    if (query["brand-id"]) {
-      const brandId = query["brand-id"];
-      delete query["brand-id"];
+    const brandId = query["brand-id"];
+    const familyId = query["family-id"];
 
-      // Apply filters to only include models with the specified brand ID
-      query.filters = {
-        $and: [
-          query.filters || {},
-          {
-            brandFamily: {
-              brands: {
-                id: brandId,
-              },
-            },
-          },
-        ],
-      };
+    // Remove the brand-id and family-id from the query parameters
+    delete query["brand-id"];
+    delete query["family-id"];
+
+    // Apply filters to only include models with the specified brand ID and family ID
+    const filters = [
+      query.filters || {},
+      ...(brandId ? [{ brandFamily: { brands: { id: brandId } } }] : []),
+      ...(familyId ? [{ brandFamily: { model_family: { id: familyId } } }] : []),
+    ];
+
+    if (filters.length > 1) {
+      query.filters = { $and: filters };
+    } else if (filters.length === 1) {
+      query.filters = filters[0];
     }
 
     ctx.query = { populate, ...ctx.query };
