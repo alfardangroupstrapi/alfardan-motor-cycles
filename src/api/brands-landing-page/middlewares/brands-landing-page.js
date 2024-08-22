63be5a6c873f6476976e04960328ddfc854d6e8b
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-08-22 21:24:27 +0300

    Update brands-landing-page.js 1

--- src/api/brands-landing-page/middlewares/brands-landing-page.js
+++ src/api/brands-landing-page/middlewares/brands-landing-page.js
@@ -92,40 +92,41 @@ const populate = {
 
 
 module.exports = (config, { strapi }) => {
-  // Add your own logic here.
-  return async (ctx, next) => {
-    strapi.log.info("In brands-landing-page middleware.");
-    ctx.query = { populate, ...ctx.query };
-
-    await next();
-  };
-};
-
-module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     strapi.log.info("In brands-landing-page-populate middleware.");
     const { query } = ctx;
     strapi.log.info(`Query before modification: ${JSON.stringify(query)}`);
 
+    const filters = [];
+
     if (query["brand-id"]) {
       const brandId = query["brand-id"];
       delete query["brand-id"];
+      filters.push({ brand: { id: brandId } });
+    }
 
-      // Apply filters to only include models with the specified brand ID
+    if (query["brand-slug"]) {
+      const brandSlug = query["brand-slug"];
+      delete query["brand-slug"];
+      filters.push({ brand: { slug: brandSlug } });
+    }
+
+    if (filters.length > 0) {
       query.filters = {
-        $and: [
-          query.filters || {},
-          {
-            brand: {
-              id: brandId,
-            },
-          },
-        ],
+        $and: [query.filters || {}, ...filters],
       };
     }
 
-    ctx.query = { populate, ...ctx.query };
+    // Add the populate object to the query
+    if (!query.populate) {
+      query.populate = populate;
+    } else {
+      query.populate = { ...query.populate, ...populate };
+    }
+
+    ctx.query = query;
     strapi.log.info(`Query after modification: ${JSON.stringify(ctx.query)}`);
     await next();
   };
 };
+
