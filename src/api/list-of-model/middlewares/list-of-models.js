06fb8903a8bb3693cc018a4bd0da21ff9c1c7135
alfardangroupstrapi <154503452+alfardangroupstrapi@users.noreply.github.com>
2024-08-22 22:21:36 +0300

    Update list-of-models.js

--- src/api/list-of-model/middlewares/list-of-models.js
+++ src/api/list-of-model/middlewares/list-of-models.js
@@ -41,16 +41,22 @@ module.exports = (config, { strapi }) => {
 
     const brandId = query["brand-id"];
     const familyId = query["family-id"];
+    const brandSlug = query["brand-slug"];
+    const familySlug = query["family-slug"];
 
-    // Remove the brand-id and family-id from the query parameters
+    // Remove the brand and family from the query parameters
     delete query["brand-id"];
     delete query["family-id"];
+    delete query["brand-slug"];
+    delete query["family-slug"];
 
     // Apply filters to only include models with the specified brand ID and family ID
     const filters = [
       query.filters || {},
       ...(brandId ? [{ brandFamily: { brands: { id: brandId } } }] : []),
       ...(familyId ? [{ brandFamily: { model_family: { id: familyId } } }] : []),
+      ...(brandSlug ? [{ brandFamily: { brands: { slug: brandSlug } } }] : []),
+      ...(familySlug ? [{ brandFamily: { model_family: { slug: familySlug } } }] : []),
     ];
 
     if (filters.length > 1) {
